811b0d817abf71a7423594a534065c42c49fb837
tuomimi <tuomimi@users.noreply.github.com>
2015-07-23 14:03:16 +0800

    Update CET4.c

--- CET4.c
+++ CET4.c
@@ -10,13 +10,17 @@ main()
 	for(i=0;i<n;i++)
 	{
            scanf("%d",&a[i]);
-           if (a[i]>=425)
-		m= 1;
-           else
-		m= 0;
+           if(a[i]>=425&&a[i]<710)
+		m=0;
+           else if(a[i]>0&&a[i]<425)
+                m=1;
+	   else
+		m=2;
 	}
-	if(m=0)
-		printf("您还未通过四级！");
-	else
-		printf("您已通过四级！");
+	switch(m)
+	{
+	  case 0:printf("您已通过四级！");break;
+	  case 1:printf("您还未通过四级！");break;
+          case 2:printf("输入错误！");break;
+	}
 }
