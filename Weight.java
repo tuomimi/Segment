cfac967cd6b26a82dec465368ad71aad26075e0b
tuomimi <tuomimi@users.noreply.github.com>
2015-07-25 11:08:09 +0800

    Update Weight.java

--- Weight.java
+++ Weight.java
@@ -1,32 +1,45 @@
-package file;
+package test;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Weight {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		File f=new File("E:\\test.txt");
-		try {
-			FileInputStream in = new FileInputStream(f);
-			byte n[] = new byte[4096];
-			int len = in.read(n);                            //从文件中读取信息
-			//System.out.println(new String(n, 0, len));
-			in.close();                                      //关闭流
-		} catch (Exception e) {
-			e.printStackTrace();
+		FileReader file=new FileReader("E:\\test.txt");
+	    BufferedReader reader = new BufferedReader(file);
+		String s="";
+		String str="";
+		while(true){
+			s = reader.readLine();
+			if (s == null){
+				break;
+			}
+			str+=s;
 		}
-		File passage = f;  
-	    Scanner scanner = new Scanner( passage );
-		String items=( passage.split("");
-		HashMap<String,Integer> map=new HashMap<String,Integer>();
-		for(String s:items){
-			if(map.containsKey(s))
-				map.put(s, map.get(s)+1);
-			else
-				map.put(s, 1);	
+		getWord(str);
+	}
+
+	private static void getWord(String s) {
+		// TODO Auto-generated method stub
+		String regex = "[A-Za-z0-9]+";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher m = pattern.matcher(s);
+		ArrayList<String>word=new ArrayList<String>();
+		while(m.find()){
+			word.add(m.group(0));
 		}
+		//System.out.println(word);
+		wordWeight(word);
+	}
+
+	private static void wordWeight(ArrayList<String> word) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> times=new ArrayList<Integer>();
+		int []weight=new int[word.size()];
+		String []firstword=new String[10];
 	}
 }
